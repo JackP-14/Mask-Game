5ac05d92986ae6b3bca6384bfb0ffe473d443fa4
Antrexx-2007 <138504567+Antrexx-2007@users.noreply.github.com>
2026-01-31 06:53:46 +0100

    hud main menu

---
+++ Mask Game/Assets/Scripts/SceneController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
