9f30766702abfbad6befb1f10db01ca860d4354a
Antrexx-2007 <138504567+Antrexx-2007@users.noreply.github.com>
2026-01-31 03:35:53 +0100

    NPC Movement test 1

---
+++ Mask Game/Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float Xpos = transform.position.x;
+        float Ypos = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
